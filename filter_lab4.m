e3cd90976abb91a33cb51ac2255921de0f993a97
moh1ka <mg5215@ic.ac.uk>
2018-02-19 20:00:10 +0000

    Update filter_lab4.m

--- filter_lab4.m
+++ filter_lab4.m
@@ -15,28 +15,27 @@ dev_sa = (10^(-sa/20));
 dev = [dev_sa dev_rp dev_sa];
 
 %firpmord estimates the order(n); frequency(fo); amplitude(ao) and weights(w)
-%required for fileter of the specification above
+%required for filter of the specification above
 [n,fo,ao,w] = firpmord(f,a,dev,fs);
 
-%firpm(N,F,A,W) returns a filter of order N+1 i.e. length (b) = N+1
-%therefore, we need a buffer of size length(b) in our C code
+%firpm(N,F,A,W) returns a filter of order N+1 
+%therefore, we need a buffer of size length N+1 in our C code
 b = firpm(n,fo,ao,w);
 
-freqz(b)
-
-
-fvtool(b);
+freqz(b);
 
 %writing buffer size and coefficient values to a 'fir_coef.txt'
-write_b = topen('fir_coef.txt','wt');
-
-%this prints out the length of the butter required for the filter in C
+write_b = fopen('fir_coef.txt','wt');
+ 
+%this prints out the length of the buffer required for the filter in C
 fprintf(write_b,'#define N %d\n', length(b));
-fprintf(write_b,'double b[] = (');
-
+%this intializes the buffer 
+fprintf(write_b,'double b[] = {');
+%------write coeffs into array-------------------------
 for i = 1: length(b)-1
     fprintf(write_b,'%e, ',b(i));
 end
-fprintf(write_b,'%c); \n', b(length(b)));
+fprintf(write_b,'%c}; \n', b(length(b)));
 fclose('all');
+
 
